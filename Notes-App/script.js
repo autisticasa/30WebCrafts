06d0f4af21eee1a525a26dbc36a8d06cb3b9a170
Larrylurex <aidylalfaiz@gmail.com>
2025-12-06 01:35:09 +0700

    Add files via upload

---
+++ Notes-App/script.js
@@ -0,0 +1,9 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", ()=>{
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+})
